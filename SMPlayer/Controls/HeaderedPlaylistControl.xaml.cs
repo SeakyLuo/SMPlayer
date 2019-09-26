80011d4e05971d257ae47dd549e06bd2871e9214
SeakyLuo <haitianluo@umail.ucsb.edu>
2019-09-26 17:34:19 +0800

    ExpressionBuilder

--- SMPlayer/Controls/HeaderedPlaylistControl.xaml.cs
+++ SMPlayer/Controls/HeaderedPlaylistControl.xaml.cs
@@ -19,10 +19,8 @@ using Microsoft.Graphics.Canvas.Effects;
 using System.Numerics;
 using Windows.UI.Composition;
 using Windows.UI.Xaml.Hosting;
-//using ExpressionBuilder;
-//using EF = ExpressionBuilder.ExpressionFunctions;
-//using CompositionSampleGallery.Shared;
-using Microsoft.Toolkit.Uwp.UI.Animations.Expressions;
+using ExpressionBuilder;
+using EF = ExpressionBuilder.ExpressionFunctions;
 
 //https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
 
@@ -125,7 +123,7 @@ namespace SMPlayer
             // Update the ZIndex of the header container so that the header is above the items when scrolling
             var headerPresenter = (UIElement)VisualTreeHelper.GetParent(PlaylistInfoGrid);
             var headerContainer = (UIElement)VisualTreeHelper.GetParent(headerPresenter);
-            Canvas.SetZIndex((UIElement)headerContainer, 1);
+            Canvas.SetZIndex(headerContainer, 1);
 
             // Get the PropertySet that contains the scroll values from the ScrollViewer
             _scrollerPropertySet = ElementCompositionPreview.GetScrollViewerManipulationPropertySet(scrollViewer);
@@ -164,81 +162,71 @@ namespace SMPlayer
             // Create a Visual for applying the blur effect
             _blurredBackgroundImageVisual = _compositor.CreateSpriteVisual();
             _blurredBackgroundImageVisual.Brush = blurBrush;
-            //_blurredBackgroundImageVisual.Size = new Vector2((float)OverlayRectangle.ActualWidth, (float)OverlayRectangle.ActualHeight);
+            _blurredBackgroundImageVisual.Size = new Vector2((float)PlaylistInfoGrid.ActualWidth, (float)PlaylistInfoGrid.ActualHeight);
 
-            //// Insert the blur visual at the right point in the Visual Tree
-            //ElementCompositionPreview.SetElementChildVisual(OverlayRectangle, _blurredBackgroundImageVisual);
+            // Insert the blur visual at the right point in the Visual Tree
+            ElementCompositionPreview.SetElementChildVisual(PlaylistInfoGrid, _blurredBackgroundImageVisual);
 
-            //// Create and start an ExpressionAnimation to track scroll progress over the desired distance
-            //ExpressionNode progressAnimation = EF.Clamp(-scrollingProperties.Translation.Y / clampSizeNode, 0, 1);
-            //_props.StartAnimation("progress", progressAnimation);
+            // Create and start an ExpressionAnimation to track scroll progress over the desired distance
+            ExpressionNode progressAnimation = EF.Clamp(-scrollingProperties.Translation.Y / clampSizeNode, 0, 1);
+            _props.StartAnimation("progress", progressAnimation);
 
-            //// Create and start an ExpressionAnimation to animate blur radius between 0 and 15 based on progress
-            //ExpressionNode blurAnimation = EF.Lerp(0, 15, progressNode);
-            //_blurredBackgroundImageVisual.Brush.Properties.StartAnimation("blur.BlurAmount", blurAnimation);
+            // Create and start an ExpressionAnimation to animate blur radius between 0 and 15 based on progress
+            ExpressionNode blurAnimation = EF.Lerp(0, 15, progressNode);
+            _blurredBackgroundImageVisual.Brush.Properties.StartAnimation("blur.BlurAmount", blurAnimation);
 
-            //// Get the backing visual for the header so that its properties can be animated
-            //Visual headerVisual = ElementCompositionPreview.GetElementVisual(Header);
+            // Get the backing visual for the header so that its properties can be animated
+            Visual headerVisual = ElementCompositionPreview.GetElementVisual(PlaylistInfoGrid);
 
-            //// Create and start an ExpressionAnimation to clamp the header's offset to keep it onscreen
-            //ExpressionNode headerTranslationAnimation = EF.Conditional(progressNode < 1, 0, -scrollingProperties.Translation.Y - clampSizeNode);
-            //headerVisual.StartAnimation("Offset.Y", headerTranslationAnimation);
+            // Create and start an ExpressionAnimation to clamp the header's offset to keep it onscreen
+            ExpressionNode headerTranslationAnimation = EF.Conditional(progressNode < 1, 0, -scrollingProperties.Translation.Y - clampSizeNode);
+            headerVisual.StartAnimation("Offset.Y", headerTranslationAnimation);
 
-            //// Create and start an ExpressionAnimation to scale the header during overpan
-            //ExpressionNode headerScaleAnimation = EF.Lerp(1, 1.25f, EF.Clamp(scrollingProperties.Translation.Y / 50, 0, 1));
-            //headerVisual.StartAnimation("Scale.X", headerScaleAnimation);
-            //headerVisual.StartAnimation("Scale.Y", headerScaleAnimation);
+            // Create and start an ExpressionAnimation to scale the header during overpan
+            ExpressionNode headerScaleAnimation = EF.Lerp(1, 1.25f, EF.Clamp(scrollingProperties.Translation.Y / 50, 0, 1));
+            headerVisual.StartAnimation("Scale.X", headerScaleAnimation);
+            headerVisual.StartAnimation("Scale.Y", headerScaleAnimation);
 
-            ////Set the header's CenterPoint to ensure the overpan scale looks as desired
-            //headerVisual.CenterPoint = new Vector3((float)(Header.ActualWidth / 2), (float)Header.ActualHeight, 0);
+            //Set the header's CenterPoint to ensure the overpan scale looks as desired
+            headerVisual.CenterPoint = new Vector3((float)(PlaylistInfoGrid.ActualWidth / 2), (float)PlaylistInfoGrid.ActualHeight, 0);
 
-            //// Get the backing visual for the photo in the header so that its properties can be animated
-            //Visual photoVisual = ElementCompositionPreview.GetElementVisual(BackgroundRectangle);
+            // Get the backing visual for the photo in the header so that its properties can be animated
+            Visual photoVisual = ElementCompositionPreview.GetElementVisual(PlaylistInfoGrid);
 
-            //// Create and start an ExpressionAnimation to opacity fade out the image behind the header
-            //ExpressionNode imageOpacityAnimation = 1 - progressNode;
-            //photoVisual.StartAnimation("opacity", imageOpacityAnimation);
+            // Create and start an ExpressionAnimation to opacity fade out the image behind the header
+            ExpressionNode imageOpacityAnimation = 1 - progressNode;
+            photoVisual.StartAnimation("opacity", imageOpacityAnimation);
 
-            //// Get the backing visual for the profile picture visual so that its properties can be animated
-            //Visual profileVisual = ElementCompositionPreview.GetElementVisual(ProfileImage);
+            // Get the backing visual for the profile picture visual so that its properties can be animated
+            Visual profileVisual = ElementCompositionPreview.GetElementVisual(PlaylistCover);
 
-            //// Create and start an ExpressionAnimation to scale the profile image with scroll position
-            //ExpressionNode scaleAnimation = EF.Lerp(1, scaleFactorNode, progressNode);
-            //profileVisual.StartAnimation("Scale.X", scaleAnimation);
-            //profileVisual.StartAnimation("Scale.Y", scaleAnimation);
+            // Create and start an ExpressionAnimation to scale the profile image with scroll position
+            ExpressionNode scaleAnimation = EF.Lerp(1, scaleFactorNode, progressNode);
+            profileVisual.StartAnimation("Scale.X", scaleAnimation);
+            profileVisual.StartAnimation("Scale.Y", scaleAnimation);
 
-            //// Get backing visuals for the text blocks so that their properties can be animated
-            //Visual blurbVisual = ElementCompositionPreview.GetElementVisual(Blurb);
-            //Visual subtitleVisual = ElementCompositionPreview.GetElementVisual(SubtitleBlock);
-            //Visual moreVisual = ElementCompositionPreview.GetElementVisual(MoreText);
+            // Get backing visuals for the text blocks so that their properties can be animated
+            Visual subtitleVisual = ElementCompositionPreview.GetElementVisual(PlaylistInfoTextBlock);
 
-            //// Create an ExpressionAnimation that moves between 1 and 0 with scroll progress, to be used for text block opacity
-            //ExpressionNode textOpacityAnimation = EF.Clamp(1 - (progressNode * 2), 0, 1);
+            // Create an ExpressionAnimation that moves between 1 and 0 with scroll progress, to be used for text block opacity
+            ExpressionNode textOpacityAnimation = EF.Clamp(1 - (progressNode * 2), 0, 1);
 
-            //// Start opacity and scale animations on the text block visuals
-            //blurbVisual.StartAnimation("Opacity", textOpacityAnimation);
-            //blurbVisual.StartAnimation("Scale.X", scaleAnimation);
-            //blurbVisual.StartAnimation("Scale.Y", scaleAnimation);
+            // Start opacity and scale animations on the text block visuals
+            subtitleVisual.StartAnimation("Opacity", textOpacityAnimation);
+            subtitleVisual.StartAnimation("Scale.X", scaleAnimation);
+            subtitleVisual.StartAnimation("Scale.Y", scaleAnimation);
 
-            //subtitleVisual.StartAnimation("Opacity", textOpacityAnimation);
-            //subtitleVisual.StartAnimation("Scale.X", scaleAnimation);
-            //subtitleVisual.StartAnimation("Scale.Y", scaleAnimation);
+            // Get the backing visuals for the text and button containers so that their properites can be animated
+            Visual textVisual = ElementCompositionPreview.GetElementVisual(PlaylistInfoGrid);
+            Visual buttonVisual = ElementCompositionPreview.GetElementVisual(PlaylistCommandBar);
 
-            //moreVisual.StartAnimation("Opacity", textOpacityAnimation);
-            //moreVisual.StartAnimation("Scale.X", scaleAnimation);
-            //moreVisual.StartAnimation("Scale.Y", scaleAnimation);
+            // When the header stops scrolling it is 150 pixels offscreen.  We want the text header to end up with 50 pixels of its content
+            // offscreen which means it needs to go from offset 0 to 100 as we traverse through the scrollable region
+            ExpressionNode contentOffsetAnimation = progressNode * 100;
+            textVisual.StartAnimation("Offset.Y", contentOffsetAnimation);
 
-            //// Get the backing visuals for the text and button containers so that their properites can be animated
-            //Visual textVisual = ElementCompositionPreview.GetElementVisual(TextContainer);
-            //Visual buttonVisual = ElementCompositionPreview.GetElementVisual(ButtonPanel);
-
-            //// When the header stops scrolling it is 150 pixels offscreen.  We want the text header to end up with 50 pixels of its content
-            //// offscreen which means it needs to go from offset 0 to 100 as we traverse through the scrollable region
-            //ExpressionNode contentOffsetAnimation = progressNode * 100;
-            //textVisual.StartAnimation("Offset.Y", contentOffsetAnimation);
-
-            //ExpressionNode buttonOffsetAnimation = progressNode * -100;
-            //buttonVisual.StartAnimation("Offset.Y", buttonOffsetAnimation);
+            ExpressionNode buttonOffsetAnimation = progressNode * -100;
+            buttonVisual.StartAnimation("Offset.Y", buttonOffsetAnimation);
         }
     }
 }
