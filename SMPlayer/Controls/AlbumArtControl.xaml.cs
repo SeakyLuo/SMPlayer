01dc863fb9ac67d2d7b7ce80d253e1c0cbd35bd5
SeakyLuo <haitianluo@umail.ucsb.edu>
2019-11-14 16:58:42 +0800

    Save Work

--- SMPlayer/Controls/AlbumArtControl.xaml.cs
+++ SMPlayer/Controls/AlbumArtControl.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 //https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
@@ -47,17 +48,29 @@ namespace SMPlayer.Controls
 
         private async void ChangeAlbumArtButton_Click(object sender, RoutedEventArgs e)
         {
-            FileOpenPicker picker = new FileOpenPicker
+            var music_file = await CurrentMusic.GetStorageFileAsync();
+            using (var dst = await music_file.GetThumbnailAsync(Windows.Storage.FileProperties.ThumbnailMode.MusicView, 1024))
             {
-                SuggestedStartLocation = PickerLocationId.PicturesLibrary
-            };
-            foreach (var item in new string[] { "*.jpg", "*.png" })
-                picker.FileTypeFilter.Add(item);
-            var file = await picker.PickSingleFileAsync();
-            if (file == null) return;
-            using (var thumbnail = await file.GetThumbnailAsync(Windows.Storage.FileProperties.ThumbnailMode.MusicView, 300))
-            {
-
+                if (!dst.CanWrite)
+                {
+                    MainPage.Instance.ShowNotification(Helper.LocalizeMessage("UnableToChangeAlbumArt"));
+                    return;
+                }
+                FileOpenPicker picker = new FileOpenPicker
+                {
+                    SuggestedStartLocation = PickerLocationId.PicturesLibrary
+                };
+                foreach (var item in new string[] { ".jpg", ".png", ".jpeg" })
+                    picker.FileTypeFilter.Add(item);
+                var file = await picker.PickSingleFileAsync();
+                if (file == null) return;
+                using (var src_stream = await file.OpenReadAsync())
+                {
+                    await dst.WriteAsync(await FileIO.ReadBufferAsync(file));
+                    var src = new BitmapImage();
+                    src.SetSource(src_stream);
+                    AlbumArt.Source = src;
+                }
             }
             //using (var tagFile = TagLib.File.Create(new MusicFileAbstraction(await CurrentMusic.GetStorageFileAsync()), TagLib.ReadStyle.Average))
             //{
