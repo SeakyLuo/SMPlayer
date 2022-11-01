32e2ee505762d39ced9d3d40f7c5003e05ccf408
SeakyLuo <464082754@qq.com>
2022-11-02 00:45:41 +0800

    add SMPlayerWeb

--- SMPlayer/RemotePlayPage.xaml.cs
+++ SMPlayer/RemotePlayPage.xaml.cs
@@ -13,6 +13,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
@@ -39,7 +40,7 @@ namespace SMPlayer
     /// </summary>
     public sealed partial class RemotePlayPage : Page
     {
-        private Socket socket;
+        private TcpListener tcpListener;
 
         public RemotePlayPage()
         {
@@ -70,7 +71,8 @@ namespace SMPlayer
                 UrlTextBox.Text = address;
                 await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
                 {
-                    //LaunchLocalServer(port);
+                    ShutdownLocalServer();
+                    LaunchLocalServer(port);
                     QRCodeImage.Source = await ImageHelper.GenQRCode(address);
                 });
             } 
@@ -78,7 +80,7 @@ namespace SMPlayer
             {
                 await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
                 {
-                    //ShutdownLocalServer();
+                    ShutdownLocalServer();
                 });
             }
         }
@@ -139,155 +141,14 @@ namespace SMPlayer
         private void LaunchLocalServer(int port)
         {
             IPAddress ip = GetIp();
-            TcpListener listener = new TcpListener(ip, port);
-            IPEndPoint endpoint = new IPEndPoint(Dns.GetHostAddresses(Dns.GetHostName())[0], port);
-            //创建服务端负责监听的套接字，参数（使用IPV4协议，使用流式连接，使用TCO协议传输数据）
-            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(endpoint);
-            if (socket.Connected)
-            {
-                Log.Info(socket.RemoteEndPoint + "连接成功");
-            }
+            tcpListener = new TcpListener(ip, port);
         }
 
         private void ShutdownLocalServer()
         {
-            if (socket == null) return;
-            socket.Close();
+            tcpListener?.Stop();
         }
 
-        /// <summary>
-        /// 向远程主机发送数据
-        /// </summary>
-        /// <param name="socket">要发送数据且已经连接到远程主机的 Socket</param>
-        /// <param name="buffer">待发送的数据</param>
-        /// <param name="outTime">发送数据的超时时间，以秒为单位，可以精确到微秒</param>
-        /// <returns>0:发送数据成功；-1:超时；-2:发送数据出现错误；-3:发送数据时出现异常</returns>
-        /// <remarks >
-        /// 当 outTime 指定为-1时，将一直等待直到有数据需要发送
-        /// </remarks>
-        public static int SendData(Socket socket, byte[] buffer, int outTime)
-        {
-            if (socket == null || socket.Connected == false)
-            {
-                throw new ArgumentException("参数socket 为null，或者未连接到远程计算机");
-            }
-            if (buffer == null || buffer.Length == 0)
-            {
-                throw new ArgumentException("参数buffer 为null ,或者长度为 0");
-            }
-
-            int flag = 0;
-            try
-            {
-                int left = buffer.Length;
-                int sndLen = 0;
-
-                while (true)
-                {
-                    if ((socket.Poll(outTime * 100, SelectMode.SelectWrite) == true))
-                    {        // 收集了足够多的传出数据后开始发送
-                        sndLen = socket.Send(buffer, sndLen, left, SocketFlags.None);
-                        left -= sndLen;
-                        if (left == 0)
-                        {                                        // 数据已经全部发送
-                            flag = 0;
-                            break;
-                        }
-                        else
-                        {
-                            if (sndLen > 0)
-                            {                                    // 数据部分已经被发送
-                                continue;
-                            }
-                            else
-                            {                                                // 发送数据发生错误
-                                flag = -2;
-                                break;
-                            }
-                        }
-                    }
-                    else
-                    {                                                        // 超时退出
-                        flag = -1;
-                        break;
-                    }
-                }
-            }
-            catch (SocketException e)
-            {
-
-                flag = -3;
-            }
-            return flag;
-        }
-
-
-        /// <summary>
-        /// 向远程主机发送文件
-        /// </summary>
-        /// <param name="socket" >要发送数据且已经连接到远程主机的 socket</param>
-        /// <param name="fileName">待发送的文件名称</param>
-        /// <param name="maxBufferLength">文件发送时的缓冲区大小</param>
-        /// <param name="outTime">发送缓冲区中的数据的超时时间</param>
-        /// <returns>0:发送文件成功；-1:超时；-2:发送文件出现错误；-3:发送文件出现异常；-4:读取待发送文件发生错误</returns>
-        /// <remarks >
-        /// 当 outTime 指定为-1时，将一直等待直到有数据需要发送
-        /// </remarks>
-        public static int SendFile(Socket socket, string fileName, int maxBufferLength, int outTime)
-        {
-            if (fileName == null || maxBufferLength <= 0)
-            {
-                throw new ArgumentException("待发送的文件名称为空或发送缓冲区的大小设置不正确.");
-            }
-            int flag = 0;
-            try
-            {
-                FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-                long fileLen = fs.Length;                        // 文件长度
-                long leftLen = fileLen;                            // 未读取部分
-                int readLen = 0;                                // 已读取部分
-                byte[] buffer = null;
-
-                if (fileLen <= maxBufferLength)
-                {            /* 文件可以一次读取*/
-                    buffer = new byte[fileLen];
-                    readLen = fs.Read(buffer, 0, (int)fileLen);
-                    flag = SendData(socket, buffer, outTime);
-                }
-                else
-                {
-                    /* 循环读取文件,并发送 */
-
-                    while (leftLen != 0)
-                    {
-                        if (leftLen < maxBufferLength)
-                        {
-                            buffer = new byte[leftLen];
-                            readLen = fs.Read(buffer, 0, Convert.ToInt32(leftLen));
-                        }
-                        else
-                        {
-                            buffer = new byte[maxBufferLength];
-                            readLen = fs.Read(buffer, 0, maxBufferLength);
-                        }
-                        if ((flag = SendData(socket, buffer, outTime)) < 0)
-                        {
-                            break;
-                        }
-                        leftLen -= readLen;
-                    }
-                }
-                fs.Flush();
-                fs.Close();
-            }
-            catch (IOException e)
-            {
-
-                flag = -4;
-            }
-            return flag;
-        }
 
     }
 }
