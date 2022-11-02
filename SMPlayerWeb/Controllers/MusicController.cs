f678b443e541302112bd4a74106df74705c6aa96
SeakyLuo <464082754@qq.com>
2022-11-02 23:52:32 +0800

    try to add MusicController

---
+++ SMPlayerWeb/Controllers/MusicController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SMPlayerWeb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MusicController : ControllerBase
+    {
+        //[HttpGet("get/{id?}")]
+        //public SMPlayer.Models.MusicView GetMusicById(long id)
+        //{
+        //    return SMPlayer.Services.MusicService.FindMusic(id);
+        //}
+    }
+}
--- SMPlayerWeb/Controllers/WeatherForecastController.cs
+++ SMPlayerWeb/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace SMPlayerWeb.Controllers
 {
@@ -28,6 +29,29 @@ namespace SMPlayerWeb.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet("{days?}")]
+        public IEnumerable<WeatherForecast> Get(int days)
+        {
+            return Enumerable.Range(1, days).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("today")]
+        public WeatherForecast GetToday()
+        {
+            return new WeatherForecast
+            {
+                Date = DateTime.Now,
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            };
         }
     }
 }
